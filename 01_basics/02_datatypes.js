a47bc9cfefafea853e8ec0ed9463ca2a6f679693
Shivam <shivamgarade05@gmail.com>
2026-08-06 14:56:48 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+// What ever need to write as a note you should write everything here not in book like normally do
+
+// previously need 
+// "use strict" - but now it is default - for new syntaxes
+
+// alert - its not part of js its browser feature - as we are using deno not browser
+
+// semicolon is not mandatory but it is recommended to use it.
+
+// console.log(3+3) console.log(4+3) - SyntaxError: Expected ';' - ans also readablilty issue
+/*
+console.log(3
++ 
+3); - this is not a good a practice , code readablilty should be high
+*/
+
+// datatypes 
+// number -> 2^53 - 1
+// bigint -> for large numbers
+// string -> "hello"
+// boolean -> true/false
+// null -> standalone "value"
+// undefined -> no value assigned
+// symbol -> unique values
+
+// object 
+
+console.log(typeof null)
+console.log(typeof "hello")
+console.log(typeof 2)
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof Symbol("hello"))
+console.log(typeof {})

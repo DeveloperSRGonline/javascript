93bd2b14d430001ee05c7168ae13562b561c427c
Shivam <shivamgarade05@gmail.com>
2026-08-13 23:26:30 +0530

    feat: add notes and examples for JavaScript object manipulation and destructuring

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -49,3 +49,76 @@ const userData = Object.keys(tinderUser)
 
 // console.log(tinderUser.hasOwnProperty("id"))
 // console.log(tinderUser.hasOwnProperty("isLoggedIn"))
+
+// destructuring of object
+const course = {
+    courseName:"js series",
+    price:"999",
+    courseInstructor:"Hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor:instructor} = course
+
+console.log(instructor)
+
+// const navbar = ({company}) => {
+//     console.log(company)
+// }
+// const comp = {company:"hiteshCompany"}
+// navbar(comp)
+
+// api - just an values comes from backend asia samjho abhi ke liye
+
+// {
+//     "name":"Antigravity",
+//     "courseName":"js in hindi series",
+//     "price":"free"
+// }
+
+// https://api.github.com/users/DeveloperSRGonline 
+
+// {
+//   "login": "DeveloperSRGonline",
+//   "id": 177343433,
+//   "node_id": "U_kgDOCpILyQ",
+//   "avatar_url": "https://avatars.githubusercontent.com/u/177343433?v=4",
+//   "gravatar_id": "",
+//   "url": "https://api.github.com/users/DeveloperSRGonline",
+//   "html_url": "https://github.com/DeveloperSRGonline",
+//   "followers_url": "https://api.github.com/users/DeveloperSRGonline/followers",
+//   "following_url": "https://api.github.com/users/DeveloperSRGonline/following{/other_user}",
+//   "gists_url": "https://api.github.com/users/DeveloperSRGonline/gists{/gist_id}",
+//   "starred_url": "https://api.github.com/users/DeveloperSRGonline/starred{/owner}{/repo}",
+//   "subscriptions_url": "https://api.github.com/users/DeveloperSRGonline/subscriptions",
+//   "organizations_url": "https://api.github.com/users/DeveloperSRGonline/orgs",
+//   "repos_url": "https://api.github.com/users/DeveloperSRGonline/repos",
+//   "events_url": "https://api.github.com/users/DeveloperSRGonline/events{/privacy}",
+//   "received_events_url": "https://api.github.com/users/DeveloperSRGonline/received_events",
+//   "type": "User",
+//   "user_view_type": "public",
+//   "site_admin": false,
+//   "name": "Shivam Garade",
+//   "company": null,
+//   "blog": "https://portfolio-shivam-garade.vercel.app",
+//   "location": null,
+//   "email": null,
+//   "hireable": null,
+//   "bio": "From Nothing heading towards Something by failing more than most people try...",
+//   "twitter_username": "srgwebsquare",
+//   "public_repos": 98,
+//   "public_gists": 0,
+//   "followers": 3,
+//   "following": 7,
+//   "created_at": "2024-08-02T15:55:42Z",
+//   "updated_at": "2026-06-21T07:18:46Z"
+// }
+
+
+// is tarike se bhi
+// [
+//     {},
+//     {},
+//     {}
+// ]

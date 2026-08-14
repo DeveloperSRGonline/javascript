ebedcdb97390c3d5e7fea1e52edeb11334bc769f
Shivam <shivamgarade05@gmail.com>
2026-08-14 11:24:12 +0530

    feat: add examples of named and unnamed IIFEs with arrow function implementation

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,18 @@
+// function chai(){
+//     console.log(`DB Connected`)
+// }
+// chai()
+
+// IIFE - Immediately Invoked Function Expression
+// isko isliye use karte hai kyuki iska Global Scope aur Function Scope ko pollution se bachata hai
+
+(function chai(){
+    // named iife
+    console.log(`DB Connected using IIFE`)
+})(); // -> ye semin color jaruri hai jab do iife ek saath jab likhna ho
+
+// arrow function example
+
+((name) => { // ye hai unnamed iife
+    console.log(`DB Connected using arrow function ${name}`)
+})("Shivam");

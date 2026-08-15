106ef2c8162632e0b83e56dd298c9a13aa7ffa32
Shivam <shivamgarade05@gmail.com>
2026-08-15 19:45:35 +0530

    feat: add two.js demonstrating while and do-while loop iterations

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+let myArray = ["superman","batman","spiderman"]
+let index = 0
+
+// while (index < myArray.length) {
+//     console.log(`${myArray[index]}`)
+//     index++
+// }
+
+let score = 11;
+
+do {
+    console.log(`score is : ${score}`)
+    score++
+} while (score <= 10);
